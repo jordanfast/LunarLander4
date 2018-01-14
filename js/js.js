5c498ba748f911d21df7bff2648a9d77e2c2a444
jordanfast <32299133+jordanfast@users.noreply.github.com>
2018-01-14 16:08:13 +0100

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,1 +1,130 @@
-function start(){timer=setInterval(function(){moverNave()},1e3*dt)}function stop(){clearInterval(timer)}function moverNave(){v+=a*dt,document.getElementById("velocidad").innerHTML=Math.round(v),y+=v*dt,document.getElementById("altura").innerHTML=Math.round(80-y),y<80?document.getElementById("nave").style.top=y+"%":(v>=5?(explosion(),activo=!1,document.getElementById("loose").style.display="inline-block",window.setTimeout(function(){document.location.reload(!0)},1e4),stop()):aterrizado(),0==fuel&&(activo=!1,timerFuel=null,stop()))}function motorOn(){1==activo&&(a=-g,document.getElementById("imgNave").src="img/cohete2.png",null==timerFuel&&(timerFuel=setInterval(function(){actualizar()},100)),fuel<=0&&(motorOff(),document.getElementById("fuel").innerHTML=0))}function motorOff(){1==activo&&(a=g,document.getElementById("imgNave").src="img/cohete.png",clearInterval(timerFuel),timerFuel=null)}function actualizar(){fuel-=1,document.getElementById("fuel").innerHTML=fuel}function aterrizado(){motorOff(),stop(),activo=!1,document.getElementById("win").style.display="inline-block"}function explosion(){document.getElementById("imgNave").src="img/explosion.gif",stop()}function pausar(){motorOff(),clearInterval(timerFuel),timerFuel=null,activo=!1,stop(),document.getElementById("pausa").style.display="none",document.getElementById("reanudar").style.display="inline-block"}function play(){activo=!0,start(),document.getElementById("pausa").style.display="inline-block",document.getElementById("reanudar").style.display="none"}function reload(){location.reload(!0)}var y=10,v=0,g=1.622,a=g,dt=.016683,timer=null,timerFuel=null,fuel=100,activo=!0;window.onload=function(){document.getElementById("showm").onclick=function(){document.getElementsByClassName("c")[0].style.display="block",stop()},document.getElementById("hidem").onclick=function(){document.getElementsByClassName("c")[0].style.display="none",start()},start(),y>0&&(document.onkeydown=motorOn,document.onkeyup=motorOff)};
+var y = 10; // altura inicial y0=10%, debe leerse del css
+var v = 0;
+var g = 1.622;
+var a = g;
+var dt = 0.016683;
+var timer = null;
+var timerFuel = null;
+var fuel = 100;
+var activo = true;
+
+
+window.onload = function(){
+    document.getElementById("showm").onclick = function () {
+		document.getElementsByClassName("c")[0].style.display = "block";
+		stop();
+	}
+	document.getElementById("hidem").onclick = function () {
+	document.getElementsByClassName("c")[0].style.display = "none";
+		start();
+	}
+	//Empezar a mover nave
+	start();
+  if (y > 0){
+
+  document.onkeydown = motorOn;
+  document.onkeyup = motorOff;
+}
+}
+
+
+
+function start(){
+	timer=setInterval(function(){ moverNave(); }, dt*1000);
+}
+
+function stop(){
+	clearInterval(timer);
+}
+
+
+function moverNave(){
+	v +=a*dt;
+	document.getElementById("velocidad").innerHTML=Math.round(v);
+	y +=v*dt;
+	document.getElementById("altura").innerHTML=Math.round(80-y);
+
+	//mover hasta que top sea un 70% de la pantalla
+	if (y<80){
+		document.getElementById("nave").style.top = y+"%";
+	} else {
+    if (v>=5){
+      explosion();
+      activo = false;
+      document.getElementById('loose').style.display = 'inline-block';
+      window.setTimeout(function(){ document.location.reload(true); }, 10000);
+
+
+      stop();
+    } else aterrizado();
+
+  if (fuel == 0){
+    activo = false;
+    timerFuel = null;
+    stop();
+  }
+	}
+}
+
+function motorOn (){
+  if (activo == true){
+  a = -g;
+  document.getElementById("imgNave").src = "img/cohete2.png";
+  if (timerFuel == null)
+      timerFuel = setInterval(function() {
+        actualizar();
+      },100);
+if (fuel <= 0 ){
+  motorOff();
+  document.getElementById("fuel").innerHTML = 0;
+}
+}
+}
+
+function motorOff (){
+  if (activo == true){
+  a = g;
+  document.getElementById("imgNave").src = "img/cohete.png";
+  clearInterval(timerFuel);
+  timerFuel = null;
+}
+}
+
+function actualizar(){
+  fuel -= 1;
+  document.getElementById("fuel").innerHTML = fuel;
+}
+
+function aterrizado(){
+  motorOff();
+  stop();
+  activo = false;
+  document.getElementById('win').style.display = 'inline-block';
+}
+
+function explosion(){
+  document.getElementById("imgNave").src = "img/explosion.gif";
+  stop();
+}
+
+
+function pausar(){
+  motorOff();
+  clearInterval(timerFuel);
+  timerFuel = null;
+  activo = false;
+  stop();
+  document.getElementById("pausa").style.display = 'none';
+  document.getElementById("reanudar").style.display = 'inline-block';
+}
+
+function play(){
+  activo = true;
+  start();
+  document.getElementById("pausa").style.display = 'inline-block';
+  document.getElementById("reanudar").style.display = 'none';
+}
+
+function reload(){
+  location.reload(true);
+}
